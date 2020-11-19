c7d95357759d0f35ef49ae4646bc5bafc0bdd2f6
MexicanOwl64 <55261799+MexicanOwl64@users.noreply.github.com>
2020-11-19 01:08:10 -0600

    added the modal popup
    The modal popup website is finally complete I still need to add the link which should come up anytime now.

---
+++ modal_popout/main.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+
+$('#showBox').click(function(){
+  $('#box-show').show()
+  $('#box-image').show()
+})
+
+
+$('#hideBox').click(function(){
+  $('#box-show').hide();
+  $('#box-image').hide();
+})
+
+});
